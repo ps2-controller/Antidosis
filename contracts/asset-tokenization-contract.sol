2aa74331cbbc4f3add6a50366faa96eea3ae291f
Anurag <anurag.angara@gmail.com>
2019-01-21 22:02:46 -0500

    harbinger transfer logic

--- contracts/asset-tokenization-contract.sol
+++ contracts/asset-tokenization-contract.sol
@@ -2,11 +2,30 @@ pragma solidity ^0.5.0;
 // import Ownable 
 import "./tokenize-core.sol";
 
+//todo:
+//add eip 161 support
+//check consistency of requiring payment denominated in payment address
+// set up distribution scheme
+// set up all necessary interfaces and stuff
+// set up duration logic
+//set up taxing logic/requirements/penalties
+//enforce appropriate permissioning for everything
+
+
+//long term todo (nice-to-haves):
+//allow the option of using eth instead of wrapped eth for underlying value
+
 contract AssetTokenizationContract is TokenizeCore, Ownable{
 
 	//contract variables
+	//the ERC721 token locked to create the shares denominated in this ERC20 token
 	UnderlyingToken public contractUnderlyingToken;
+	// the address that contains deployment logic for the initial distribution of tokens
 	address distributionAddress;
+	// the token in which payments for shares are to be made
+	address paymentAddress;
+	address tokenizeCore;
+
 	uint256 public totalSupply;
 	string public name;
 	string public symbol;
@@ -14,36 +33,50 @@ contract AssetTokenizationContract is TokenizeCore, Ownable{
 	uint taxRate;
 	uint defaultValue = 0;
 	uint defaultDuration = 0;
+	uint minimumShares;
 
 	//structs
-	struct UnderLyingToken{
+	struct UnderlyingToken{
 		address underlyingTokenAddress;
 		uint underlyingTokenId;
 	}
 
+	struct UserTaxBreakdown{
+		uint userDebt;
+		uint userReleased;
+	}
+
 	struct HarbingerSet{
 		uint userValue;
+		//duration in seconds
 		uint userDuration;
 		bool initialized;
 	}
 
+
 	//mappings
 	mapping (address => uint256) balances;
     mapping (address => mapping (address => uint256)) allowed;
-    mapping(address => HarbingerSet) harbingerSetByUser;
+    mapping (address => HarbingerSet) harbingerSetByUser;
+    mapping (address => UserTaxBreakdown) taxBreakdownByUser;
 
 	constructor AssetTokenizationContract is ERC20, TokenizeCore (
 		address _underlyingTokenAddress, 
 		address _distributionAddress,
+		address _paymentAddress,
 		uint _underlyingTokenId,
 		uint256 _erc20Supply, 
 		string _erc20Name, 
 		string _erc20Symbol, 
-		uint _erc20Decimals,  
+		uint _erc20Decimals,
+		uint _minimumShares,  
 		bytes _deploymentData)
 	{
-		UnderlyingToken.underlyingTokenAddress = _underlyingTokenAddress;
-		UnderlyingToken.underlyingTokenId = _underlyingTokenId;
+		tokenizeCore == msg.sender;
+		paymentAddress = _paymentAddress;
+		minimumShares = _minimumShares;
+		contractUnderlyingToken.underlyingTokenAddress = _underlyingTokenAddress;
+		contractUnderlyingToken.underlyingTokenId = _underlyingTokenId;
 		totalSupply = _erc20Supply;
 		name = _erc20Name;
 		symbol = _erc20Symbol;
@@ -57,7 +90,8 @@ contract AssetTokenizationContract is TokenizeCore, Ownable{
 
 	function transfer(address _to, uint256 _value) returns (bool success) {
         // makes sure that once the underlying asset is unlocked, the tokens are destroyed
-        require (msg.sender != underlyingToken);
+        require (msg.sender != tokenizeCore);
+        require (balances[_to] + _value >= minimumShares && balances[msg.sender - _value >= minimumShares]);
         require (harbingerSetByUser[_to].initialized == true);
         if (balances[msg.sender] >= _value && _value > 0) {
             balances[msg.sender] -= _value;
@@ -69,14 +103,23 @@ contract AssetTokenizationContract is TokenizeCore, Ownable{
 
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
     	require (_from != underlyingToken);
-        require (harbingerSetByUser[_to].initialized == true);
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
+    	require (balances[_to] + _value >= minimumShares && (balances[_from] - _value >= minimumShares || balances[_from] - _value == 0));
+        require (harbingerSetByUser[_to].initialized == true || _to == underlyingToken);
+        require (paymentAddress.transferFrom(_to, _from, (harbingerSetByUser[_from].userValue*_value) || msg.sender == address(this), "Unable to make payment");
+        paymentAddress.transferFrom(_to, address(this), (harbingerSetByUser.userValue * harbingerSetByUser.userDuration * _value));
+        debtByUser[_to] = taxBreakdownByUser[_to].userDebt + harbingerSetByUser.userValue * harbingerSetByUser.userDuration * _value);
+        require (paymentAddress.transferFrom())
+        if (balances[_from] >= _value && _value > 0) {
             balances[_to] += _value;
             balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
             Transfer(_from, _to, _value);
             return true;
         } else { return false; }
+    }
+
+    //can be used if paymentAddress has approveAndCall
+    function allowAndTransferFrom(address _from, address _to, uint256 _value, bytes _extraData){
+
     }
 
     function balanceOf(address _owner) constant returns (uint256 balance) {
@@ -94,14 +137,14 @@ contract AssetTokenizationContract is TokenizeCore, Ownable{
     }
 
 
-        function () {
-        //if ether is sent to this address, send it back.
-        throw;
+    function () {
+    //if ether is sent to this address, send it back.
+    throw;
     }
 
-        function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
+    	allowed[msg.sender][_spender] = _value;
+    	Approval(msg.sender, _spender, _value);
 
         //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
         //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
@@ -110,14 +153,21 @@ contract AssetTokenizationContract is TokenizeCore, Ownable{
         return true;
     }
 
-    	function setHarbinger(_userValue, _userDuration) {
+   	function setHarbinger(_userValue, _userDuration) {
 		require (_userValue != 0);
 		require (_userDuration != 0);
+
 		if(harbingerSetByUser[msg.sender].initialized == false){
 			harbingerSetByUser[msg.sender].initialized == true;
 		}
 		harbingerSetByUser[msg.sender].userValue = _userValue;
 		harbingerSetByUser[msg.sender].userDuration = _userDuration;
+	}
+
+	function unlockToken(){
+		require (balances[msg.sender] == totalSupply);
+		transferFrom(msg.sender, tokenizeCore, totalSupply);
+		tokenizeCore.unlockToken(UnderlyingToken.underlyingTokenAddress, UnderlyingToken.underlyingTokenId, msg.sender);
 	}
 
 }
--- contracts/tokenize-core.sol
+++ contracts/tokenize-core.sol
@@ -14,7 +14,7 @@ interface ERC721TokenReceiver {
     function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes _data) external returns(bytes4);
 }
 	//state variables
-	//TokenToLock[] locked721Tokens;
+	TokenToLock[] locked721Tokens;
 
 	//structs
 	struct TokenToLock{
@@ -26,24 +26,41 @@ interface ERC721TokenReceiver {
 	mapping(bytes32 => address) tokenToERC20;
 
 	function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes _data) external returns(bytes4){
-		(address memory _distributionAddress, uint256 memory _erc20Supply,string memory _erc20Name, string memory _erc20Symbol, uint memory _erc20Decimals, address memory _erc20DeploymentAddress, bytes memory _deploymentData) = abi.decode(_data, (address, uint256, string, string, uint, address, bytes));
-		require(lock721Token(_operator, _distributionAddress, _tokenId, _erc20Supply, _erc20Name, _erc20Symbol, _erc20Decimals, _erc20DeploymentAddress, _deploymentData), "Error receiving token");
+		(address memory _distributionAddress, 
+			address memory _paymentAddress, 
+			uint256 memory _erc20Supply,
+			string memory _erc20Name, 
+			string memory _erc20Symbol, 
+			uint memory _erc20Decimals,
+			uint memory _minimumShares, 
+			bytes memory _deploymentData) = abi.decode(_data, (
+				address, 
+				address, 
+				uint256, 
+				string, 
+				string, 
+				uint, 
+				uint,
+				bytes));
+		require(lock721Token(_operator, _distributionAddress, _paymentAddress, _tokenId, _erc20Supply, _erc20Name, _erc20Symbol, _erc20Decimals, _erc20DeploymentAddress, _minimumShares, _deploymentData), "Error receiving token");
 		return bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"));
 	}
 
 	function lock721Token internal (
 		address _tokenToLockAddress, 
 		address _distributionAddress, 
+		address _paymentAddress,
 		uint256 _tokenToLockId, 
 		uint256 _erc20Supply, 
 		string _erc20Name, 
 		string _erc20Symbol, 
-		uint _erc20Decimals,   
+		uint _erc20Decimals,
+		uint _minimumShares,   
 		bytes _deploymentData) 
 	{
 		TokenToLock _tokenToLock = TokenToLock(_tokenToLockAddress, _tokenToLockId);
-		//locked721Tokens.push(_tokenToLock);
-		AssetTokenizationContract newAssetTokenizationContract = new AssetTokenizationContract(_tokenToLockAddress, _distributionAddress, _tokenToLockId, _erc20Supply, _erc20Name, _erc20Symbol, _erc20Decimals, _deploymentData);
+		locked721Tokens.push(_tokenToLock);
+		AssetTokenizationContract newAssetTokenizationContract = new AssetTokenizationContract(_tokenToLockAddress, _distributionAddress, _paymentAddress,	 _tokenToLockId, _erc20Supply, _erc20Name, _erc20Symbol, _erc20Decimals, _minimumShares, _deploymentData);
 		ERC20ToToken[_tokenToLock] = address(newAssetTokenizationContract);
 		bytes32 memory _tokenToLockHash = abi.encode(keccak256(_tokenToLockAddress, _tokenToLockId));
 		tokenToERC20[_tokenToLockHash] = address(newAssetTokenizationContract);
@@ -55,7 +72,7 @@ interface ERC721TokenReceiver {
 		return tokenToERC20[_tokenToLockHash];
 	}
 
-	function unfreezeToken(address _tokenToUnlockAddress, uint _tokenToUnlockId, address _claimant){
+	function unlockToken(address _tokenToUnlockAddress, uint _tokenToUnlockId, address _claimant){
 		require (msg.sender == tokenToERC20[abi.encode(keccak256(_tokenToUnlockAddress, _tokenToUnlockId))])
 		_tokenToUnlockAddress.safeTransferFrom(address(this), _claimant, _tokenToUnlockId);
 	}
